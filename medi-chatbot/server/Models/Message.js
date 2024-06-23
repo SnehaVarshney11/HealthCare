e35f6297db8136e642d994ff57e13298b80b1129
sneha <sneha.varshney_cs19@gla.ac.in>
2024-06-23 14:31:32 +0530

    Message model

---
+++ medi-chatbot/server/Models/Message.js
@@ -0,0 +1,12 @@
+// models/Message.js
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  user: String,
+  text: String,
+  timestamp: { type: Date, default: Date.now },
+});
+
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = Message;
